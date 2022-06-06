0ba69b3c82ba9ac6e518b221163f94f8e2a9a55e
Miguel Angel <miguel@gmail.com>
2022-06-05 20:51:21 -0500

    Third Commit

---
+++ app/src/main/java/com/medina/pokertinker/Data/network/PokemonService.kt
@@ -0,0 +1,16 @@
+package com.medina.pokertinker.Data.network
+
+import com.medina.pokertinker.Data.model.PokemonListModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import javax.inject.Inject
+
+class PokemonService @Inject constructor(private val pokemonApi: PokemonApi) {
+    suspend fun getPokemons() : PokemonListModel {
+        return withContext(Dispatchers.IO) {
+            val res: Response<PokemonListModel> = pokemonApi.getPokemons()
+            res.body()!!
+        }
+    }
+}
